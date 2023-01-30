531e85a515d2fdca556d55d4318d92175fced263
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-01-30 14:56:16 +0000

    cambiar tipo de dato casting

---
+++ semana3/programa4.py
@@ -0,0 +1,19 @@
+"""
+    Programa4
+    Nombre: Dulce Daniela MM
+    Fecha: 30/01/2023
+    Descripcion: cambiar tipo de dato casting
+"""
+numero1 = input("Numero1: ")
+numero2 = 10
+suma = int(numero1) + numero2
+resta = int(numero1) - numero2
+multiplicacion = int(numero1) * numero2
+division = int(numero1) / numero2
+potencia = int(numero1) ^ numero2
+print(suma)
+print (resta)
+print(multiplicacion)
+print(division)
+print(potencia)
+
