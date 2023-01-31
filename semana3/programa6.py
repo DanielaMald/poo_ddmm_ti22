91f98b1ce315c8def722a1f567903095932129f0
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-01-31 14:58:35 +0000

    área y perímetro del círculo y cuadrado

---
+++ semana3/programa6.py
@@ -0,0 +1,26 @@
+"""
+    Programa6
+    Nombre: Dulce Daniela MM
+    Fecha: 31/01/2023
+    Descripcion: area y perimetro del circulo y cuadrado
+"""
+#area del circulo
+print ("Area del circulo")#imprime en la pantalla para poder identificarlo
+PI = (3.1416)
+radio = float(input("escribe el valor del radio: "))
+area_circulo = PI * (radio ** 2) #realiza la operacion 
+print("El area del circulo con un radio de {} es {}".format(radio,area_circulo)) #imprime el area del circulo
+
+#perimetro del circulo
+print("perimetro del circulo")#imprime el perimetro del circulo
+diametro = float(input("Escribe el valor del diametro: "))#escribe el valor del diametro del circulo
+perimetro = (diametro * PI) #realiza la operacion para obtener el perimetro
+print("El perimetro del circulo es: " 
+, perimetro)#imprime el resultado
+
+#area del cuadrado
+print("Area del cuadrado")#imprime el area del cuadrado
+lado = float(input("Escribe el valor del lado: "))#escribe las medidas del lado
+area_cuadrado = float(input())
+
+
