3ff46c45cced3fecbc720cce38a090950c9c7a1c
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-01-31 13:51:27 +0000

    base, altura y perímetro del triangulo

--- semana3/programa5.py
+++ semana3/programa5.py
@@ -4,6 +4,17 @@
     Fecha: 31/01/2023
     Descripcion: base y altura
 """
-input("base:")
-input("altura:")
-print(base * altura / 2)
+#area
+print("area del triangulo")#imprimir el area del triangulo 
+altura = int(input("Escribe la altura  del triangulo: "))#pide la altura del triangulo
+base = int(input("escribe la base del triangulo: "))#introduce la base del triangulo
+area = (base * altura) / 2 #realiza la operacion 
+print(area) #imprime el area 
+
+#perimetro
+print("perimetro del triangulo")#imprime el texto para poder identificarlo 
+lado1 = int(input("Escribe el valor del lado 1: "))#pide valor del lado 1
+lado2 = int(input("escribe el valor del lado 2: "))#pide valor del lado 2
+lado3 = int(input("escribe el valor del lado 3: "))#pide valor del lado 3
+perimetro = (lado1 + lado2 + lado3)#realiza la operacion 
+print(perimetro)#imprime el valor del perimetro
