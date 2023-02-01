3fc9d91a5d7a334cb543e3dd7fa3137c1c56b751
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-02-01 14:54:19 +0000

    comparar 2 números enteros e imprimir el número mayor

---
+++ semana3/programa7.py
@@ -0,0 +1,15 @@
+"""
+    Programa7
+    Nombre: Dulce Daniela MM
+    Fecha: 01/02/2023
+    Descripcion: comparar 2 numeros enteros e imprimir el numero mayor
+"""
+numero1 = int(input("introduce el numero 1: "))
+numero2 = int(input("introduce el numero 2: "))
+if numero1 > numero2:
+  print (numero1)
+
+elif numero2> numero1:
+  print( numero2)
+elif numero1 == numero2:
+    print("ERROR")
