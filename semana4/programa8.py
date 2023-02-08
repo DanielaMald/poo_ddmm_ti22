a938e889e742cb2d70bb50ef346de656fcb367e2
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-02-08 04:14:50 +0000

    11 formulas completas

--- semana4/programa8.py
+++ semana4/programa8.py
@@ -4,13 +4,117 @@
     Fecha: 07/02/2023
     Descripcion: 11 formas diferentes
 """
-#formula 1
 n1 = int(input("n1"))
 n2 = int(input("n2"))
 
+#formula 1
 if n1 > n2 :
     print (n1)
 
 if n2 > n1:
     print (n2)
+
+if  n1 = = n2
+     print (None)
+
+ #formula 2
+if n2 > n1: 
+    print(n2)
+
+if n1 > n2:
+    print (n1)
     
+if n2 = = n1:
+    print(None)
+
+ #formula 3
+if n1 > n2:
+    print (n1)
+
+elif n2 > n1:
+    print (n2)
+
+else:
+    print(None)
+
+ #formula 4
+if n1 < n2:
+    print(n2)
+
+elif n2 < n1:
+    print(n1)
+
+else:
+    print (None)
+
+ #formula 5
+ if n2 < n1:
+     print(n1)
+
+ if n1 < n2:
+     print(n2)
+
+if n1 = = n2:
+    print(None)
+
+ #formula 6
+if n1 >= n2:
+    if n1 > n2:
+        print(n1)
+
+    else:
+        print(None)
+
+    else:                                                            print(n2)
+
+ #formula 7
+if n1 <= n2:
+    print (n2)
+
+else:
+    print(None)
+
+else:
+     print(n1)
+
+ #formula 8
+if n1 <= n2
+   if n1 = = n2:
+     print(None)
+else: 
+    print(n2)
+
+else:
+    print(n1)
+
+ #formula 9
+ if n1 = = n2:
+    print(None)
+
+elif n1 > n2:
+     print(n1)
+
+else:
+    print(n2)
+
+ #formula 10
+if n1 = = n2:
+    print (None)
+
+elif n1 < n2:
+    print(n2)
+
+else:
+print (n1)
+
+ #formula 11
+if n1 = = n2:
+   print(None)
+
+elif n1 >= n2:
+    print(n1)
+
+else:
+    print(n2)
+
+        
