1af6f25e4cb56a7605fd99a9e5b443484c04fe19
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-02-15 14:57:19 +0000

    cambiar tipo de dato

--- semana3/programa4.py
+++ semana3/programa4.py
@@ -4,16 +4,19 @@
     Fecha: 30/01/2023
     Descripcion: cambiar tipo de dato casting
 """
-numero1 = input("Numero1: ")
-numero2 = 10
-suma = int(numero1) + numero2
+numero1 = input("Numero1: ") #introduce cualquier valor
+numero2 = 10 #
+suma = int(numero1) + numero2 #colocar el valor de la variable
 resta = int(numero1) - numero2
-multiplicacion = int(numero1) * numero2
-division = int(numero1) / numero2
-potencia = int(numero1) ^ numero2
-print(suma)
-print (resta)
-print(multiplicacion)
-print(division)
+#Realiza modificaciones a cada variable dependiendo la funcion que quiera que haga
+
+multiplicacion = int(numero1) * numero2 #se realiza una multiplicacion
+
+division = int(numero1) / numero2 #se realiza una potencia
+potencia = int(numero1) ^ numero2 #realiza una potencia
+print(suma) #imprime el resultado de la suma
+print (resta) #imprime el resultado de la resta 
+print(multiplicacion) #imprime e resultado de la multiplicacion
+print(division) #imprime el resultado de la division
 print(potencia)
 
