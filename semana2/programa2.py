75784519029619170f1fb3b4e4af56e841f21ba9
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-02-15 14:47:07 +0000

    concatenar variables, formatear e identificar errores de sintaxis

--- semana2/programa2.py
+++ semana2/programa2.py
@@ -2,12 +2,10 @@
     Programa2
     Nombre: Dulce Daniela MM
     Fecha: 24/01/2023
-    Descripcion: concatenar variables, 
-     formatear e identificar errores de 
-      sintaxis 
+    Descripcion: concatenar variables,formatear e identificar errores de sintaxis 
     """
 
 variable1 = "Hola " #esta definiendo una variable de tipo string o texto
 variable2 = 619 # es una variable de tipo string
 #print(variable1 + variable2) #se suma e imprime las variables
-print("{} {}".format(variable1, variable2)) #
+print("{} {}".format(variable1, variable2)) #imprime la respuesta de la suma de ambas variables 
