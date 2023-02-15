a5de7a1679ca234a4f01edc74209b68b3c4391a2
DanielaMald <122801429+DanielaMald@users.noreply.github.com>
2023-02-15 14:39:53 +0000

    comentarios multilínea, una linea y concatenación de 2 variables

--- semana2/programa1.py
+++ semana2/programa1.py
@@ -2,8 +2,8 @@
     Programa1
     Nombre: Dulce Daniela MM
     Fecha: 23/01/2023
-    Descripcion: comentarios multilinea,      una linea y concatenacion de 2            variables
+    Descripcion: comentarios multilinea,una linea y concatenacion de 2 variables
 """
 variable1= "Hola " #  literal para almacenar una cadena de caracteres
 variable2= "mundo python" #  variable para almacenar una cadena de caracteres 
-print(variable1 + variable2)
+print(variable1 + variable2) #imprime el resultado de la suma de las variables 
